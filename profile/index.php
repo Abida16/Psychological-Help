cd81a244571a5866ba674067806b1e73866a034e
Abida16 <abida1616@gmail.com>
2017-05-17 02:02:45 +0600

    profile

--- profile/index.php
+++ profile/index.php
@@ -66,7 +66,7 @@ if(isset( $_SESSION["state"])){
           <div id="login">
               <h1>Welcome Back!</h1>
 
-              <form  id="loginform" method="post">
+              <form onsubmit="return false" id="loginform" method="post">
 
                   <div class="field-wrap">
                       <label>
